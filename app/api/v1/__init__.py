bd9c18d37fdd2193f000b430e51cfcf60c992283
Adrián Félix <guerofelix234@gmail.com>
2025-12-10 16:25:17 -0600

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import health, transactions
+from app.api.v1.endpoints import health, transactions, budgets
 
 api_router = APIRouter()
 
 api_router.include_router(health.router, tags = ["Health"])
 api_router.include_router(transactions.router, prefix = "/transactions", tags = ["transactions"])
+api_router.include_router(budgets.router, prefix = "/budgets", tags = ["budgets"])
